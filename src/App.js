f4000d8da7ef228a5c99ed38b594f17fb9512589
Rimgaile Sinkeviciene <rimgaile.sinkeviciene@gmail.com>
2018-10-19 09:35:29 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>
+            Front End Web Developer & Designer
+          </h1>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            View portfolio
           </a>
         </header>
       </div>
