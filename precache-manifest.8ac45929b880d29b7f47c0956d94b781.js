29e58aedfb364eaff75030db0aa6dd5453e078ae
Rimgaile Sinkeviciene <rimgaile.sinkeviciene@gmail.com>
2018-10-19 09:37:13 +0300

    Updates

---
+++ precache-manifest.8ac45929b880d29b7f47c0956d94b781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "c732c48e2a2469b844c3",
+    "url": "/portfolio/static/js/main.c732c48e.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/portfolio/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "c732c48e2a2469b844c3",
+    "url": "/portfolio/static/css/main.89b670ab.chunk.css"
+  },
+  {
+    "revision": "bbb125befc960272d294cbecdd8e1b92",
+    "url": "/portfolio/index.html"
+  }
+];
