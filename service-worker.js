bc8804697946e8a6265f21011ee6f8daf35a95bf
singail <rimgaile.sinkeviciene@gmail.com>
2019-03-08 09:39:37 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.056f2827602b36d0f972b8aba7d7305e.js"
+  "/portfolio/precache-manifest.e8dbaa11d2fefa2ac15cfb40e2496ba6.js"
 );
 
 workbox.clientsClaim();
