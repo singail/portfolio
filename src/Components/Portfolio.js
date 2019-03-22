cdc4fb6d9b30ac6a5b2315102c67af96c3a397f0
singail <rimgaile.sinkeviciene@gmail.com>
2019-03-22 09:25:00 +0200

    Add info about projects

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,29 +13,29 @@ const projects = [
         'url': require('../images/pokalbis.png'),
         'alt': 'web developement',
         'title': 'Asmeninis pokalbis',
-        'content': 'Info',
-        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
+        'content': 'This project was a part of a job interview of a company that hired me to build some projects. The design was created by a designer. My task was to build a website with a given design.',
+        'services': ['Wordpress CMS', 'Responsive Layout', 'Contact Form', 'Multilingual']
     },
     {
         'url': require('../images/vilniusMap.png'),
         'alt': 'web developement',
         'title': 'Vilnius map',
-        'content': 'Info about project',
-        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
+        'content': 'This project is a part of the Front-End Web Developer Nanodegree Program of Udacity. My goal was to develop a single page Neighborhood Map application using React framework. The app contains the list of places to visit in my chosen area and a map of that area with marked locations.',
+        'services': ['ReactJs', 'Website Design', 'Responsive Layout']
     },
     {
         'url': require('../images/gile.png'),
         'alt': 'web developement',
         'title': 'Gilė',
-        'content': 'Info about project',
-        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
+        'content': 'This is my first website.',
+        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout', 'SSL integration']
     },
     {
         'url': require('../images/cpto.png'),
         'alt': 'web developement',
         'title': 'Consulens baltic',
         'content': 'Info about project',
-        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
+        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout', 'Multilingual', 'SSL integration']
     },
     {
         'url': require('../images/grindu-slifavimas.png'),
@@ -49,7 +49,7 @@ const projects = [
         'alt': 'web developement',
         'title': 'GPAIS',
         'content': 'Info about project',
-        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
+        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout', 'SEO', 'SSL integration', 'Popup Contact Form']
     },
     {
         'url': require('../images/kelmesasc.png'),
@@ -62,15 +62,15 @@ const projects = [
         'url': require('../images/restaurant.png'),
         'alt': 'web developement',
         'title': 'Restaurant app',
-        'content': 'Info about project',
-        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
+        'content': 'This project is a part of the Front-End Web Developer Nanodegree Program of Udacity. For the Restaurant Reviews project, I had to convert a static webpage to a responsive web application. To complete the task I had to make the webpage and all its contents visible at any viewport(desktop, mobile, tablet). I also made the site accessible for screen reader use and added a service worker so that all visited pages would be accessible offline.',
+        'services': ['Responsive Layout', 'Accessible For Screen Reader', 'Service Worker']
     },
     {
         'url': require('../images/memoryGame.png'),
         'alt': 'web developement',
         'title': 'Memory game',
-        'content': 'Info about project',
-        'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
+        'content': 'This project is a part of the Front-End Web Developer Nanodegree Program of Udacity. As a student I received a starter code with HTML and CSS to display a static version of the Memory Game project. My goal was to add functionality and make the game interactive by using JavaScript, some HTML and CSS.',
+        'services': ['Javascript', 'HTML', 'CSS', 'Responsiveness']
     },
 ]
 
