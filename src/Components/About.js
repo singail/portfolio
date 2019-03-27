01123c3121cc7edbb130325658ff1f4d57e88c02
singail <rimgaile.sinkeviciene@gmail.com>
2019-03-27 10:26:40 +0200

    Edit content and some styles

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,7 @@ import '../App.css';
 class About extends Component {
     render() {
         return (
-            <section id="about-me" className="about">
+            <section id="about-me" className="about" style={{backgroundImage: 'url(' + require('../images/bg3.png') + ')'}}>
             <div className="about-me">
                 <img src={require("../images/profile-photo.png")} alt='web developer profile'></img>
                 <div className="about-me-introduction">
@@ -13,8 +13,10 @@ class About extends Component {
                     Hi! My name is Rimgaile. My journey as a front-end developer started in 2017, when I finished Multimedia studies in Kaunas information technology school, were I learned both graphic design and web developement.
                     </p>
                     <p className='about-content'>
-                    2017 October I was accepted as a recepient of the Google Developer Challenge Scholarship and after three months of the challenge I was selected to finish a full program of Front End Web Developer Nanodegree at Udacity. It took six months of intense studying, 8 projects were both challenging and exciting. 
+                    2017 October I was accepted as a recepient of the <strong>Google Developer Challenge Scholarship</strong> and after three months of the challenge I was selected to finish a full program of <strong>Front End Web Developer Nanodegree at Udacity</strong>. It took six months of intense studying, 8 projects were both challenging and exciting. Some of my projects are presented on my portfolio site. 
                     </p>
+                    <p className='about-content'>Most of my websites are build with <strong>Wordpress CMS</strong> (Content Management System). Wordpress is the most popular CMS that allows users to easily create, publish, edit and organize content. I help my clients to learn and use Wordpress.</p>
+                    <p className='about-content'>This website is build with <strong>React</strong> - a Javascript library for building user interfaces. </p>
                 </div>
             </div>
         </section>
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,70 +3,80 @@ import '../App.css';
 
 const projects = [
     {
-        'url': require('../images/stm.png'),
+        'img': require('../images/stm.png'),
+        'url': 'http://stnamai.lt/',
         'alt': 'web developement',
         'title': 'Skaitmeninės tekstilės namai',
         'content': 'I created this website for my friend who owns a digital textile business. This was my first website created from scratch. At first I created the design, then wrote code and added it to the Wordpress CMS. ',
         'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
     },
     {
-        'url': require('../images/pokalbis.png'),
+        'img': require('../images/pokalbis.png'),
+        'url': '#',
         'alt': 'web developement',
         'title': 'Asmeninis pokalbis',
         'content': 'This project was a part of a job interview of a company that hired me to build some projects. The design was created by a designer. My task was to build a website with a given design.',
         'services': ['Wordpress CMS', 'Responsive Layout', 'Contact Form', 'Multilingual']
     },
     {
-        'url': require('../images/vilniusMap.png'),
+        'img': require('../images/vilniusMap.png'),
+        'url': 'https://singail.github.io/Neighborhood-Map/',
         'alt': 'web developement',
         'title': 'Vilnius map',
         'content': 'This project is a part of the Front-End Web Developer Nanodegree Program of Udacity. My goal was to develop a single page Neighborhood Map application using React framework. The app contains the list of places to visit in my chosen area and a map of that area with marked locations.',
         'services': ['ReactJs', 'Website Design', 'Responsive Layout']
     },
     {
-        'url': require('../images/gile.png'),
+        'img': require('../images/gile.png'),
+        'url': 'https://gile.lt',
         'alt': 'web developement',
         'title': 'Gilė',
         'content': 'This is my first website.',
         'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout', 'SSL integration']
     },
     {
-        'url': require('../images/cpto.png'),
+        'img': require('../images/cpto.png'),
+        'url': 'https://cpto.eu',
         'alt': 'web developement',
         'title': 'Consulens baltic',
         'content': 'Info about project',
         'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout', 'Multilingual', 'SSL integration']
     },
     {
-        'url': require('../images/grindu-slifavimas.png'),
+        'img': require('../images/grindu-slifavimas.png'),
+        'url': 'https://grindu-slifavimas.lt',
         'alt': 'web developement',
         'title': 'Grindų šlifavimas',
         'content': 'Info about project',
         'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
     },
     {
-        'url': require('../images/gpais.png'),
+        'img': require('../images/gpais.png'),
+        'url': 'https://gpais.com',
         'alt': 'web developement',
         'title': 'GPAIS',
         'content': 'Info about project',
         'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout', 'SEO', 'SSL integration', 'Popup Contact Form']
     },
     {
-        'url': require('../images/kelmesasc.png'),
+        'img': require('../images/kelmesasc.png'),
+        'url': 'http://kelmesasc.lt',
         'alt': 'web developement',
         'title': 'Kelmės aklųjų ir silpnaregių centras',
         'content': 'Info about project',
         'services': ['Wordpress CMS', 'Website Design', 'Responsive Layout']
     },
     {
-        'url': require('../images/restaurant.png'),
+        'img': require('../images/restaurant.png'),
+        'url': '#',
         'alt': 'web developement',
         'title': 'Restaurant app',
         'content': 'This project is a part of the Front-End Web Developer Nanodegree Program of Udacity. For the Restaurant Reviews project, I had to convert a static webpage to a responsive web application. To complete the task I had to make the webpage and all its contents visible at any viewport(desktop, mobile, tablet). I also made the site accessible for screen reader use and added a service worker so that all visited pages would be accessible offline.',
         'services': ['Responsive Layout', 'Accessible For Screen Reader', 'Service Worker']
     },
     {
-        'url': require('../images/memoryGame.png'),
+        'img': require('../images/memoryGame.png'),
+        'url': 'https://singail.github.io/fend-project-memory-game/',
         'alt': 'web developement',
         'title': 'Memory game',
         'content': 'This project is a part of the Front-End Web Developer Nanodegree Program of Udacity. As a student I received a starter code with HTML and CSS to display a static version of the Memory Game project. My goal was to add functionality and make the game interactive by using JavaScript, some HTML and CSS.',
@@ -82,7 +92,6 @@ class Portfolio extends Component {
 
     handleClick = (e) => { 
         let projectId = e.target.getAttribute('id');
-        console.log(projectId);
         this.setState({id: projectId});
     }
 
@@ -92,7 +101,7 @@ class Portfolio extends Component {
                 {projects.map((project, i) => (
                     <div className="project-image" key={i}>
                         <div className='overlay'>
-                                <img src={project.url} alt={project.alt} onClick={this.handleClick} id={i}></img>
+                                <img src={project.img} alt={project.alt} onClick={this.handleClick} id={i}></img>
                         </div>
                     </div>
                 ))}
@@ -100,10 +109,9 @@ class Portfolio extends Component {
                         <span className='close-modal'>&times;</span>
                         <div className='modal-content'>
                             <div className='modal-images'>
-                                <a href="http://stnamai.lt/" target="_blank" rel="noopener noreferrer">
-                                    <img src={projects[this.state.id].url} alt="website design"></img>
+                                <a href={projects[this.state.id].url} target="_blank" rel="noopener noreferrer">
+                                    <img src={projects[this.state.id].img} alt="website design"></img>
                                 </a>
-                                <img src={projects[this.state.id].url} alt="web design"></img>
                             </div>
                             <div className='about-project'>
                                 <h2 className='about-project_title'>{projects[this.state.id].title}</h2>
